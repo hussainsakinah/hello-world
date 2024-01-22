cb2097fefe65482bc98550f6b709165ca3b8d276
hussainsakinah <153793583+hussainsakinah@users.noreply.github.com>
2024-01-22 10:36:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class = "right-align">
+      <h1>Roll No - 2211CS010513</h1>
+      <h1>Name - Sakinah Hussain</h1>
+      <h1>CGPA - 9.18</h1>
+
+    </div>
+  );
+}
+
+export default App;
